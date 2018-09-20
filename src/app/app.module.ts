12a7829aa96c75796f74b4356b00160f029b2ffc
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-09-20 15:24:43 -0500

    Se crea la vista principal. Se agrega la navbar en la vista de muro. Aun sin conectar vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { WallComponent } from './wall/wall.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    WallComponent
   ],
   imports: [
     BrowserModule
