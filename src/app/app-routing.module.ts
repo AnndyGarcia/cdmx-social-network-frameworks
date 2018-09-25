0825f4384108a1f730356cfe2432e0ac6ba1d8b8
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-09-25 17:36:43 -0500

    Modificacion de la ruta home, se agrega la ruta del perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { WallComponent } from './wall/wall.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  {path: 'wall',  component: WallComponent}
+  {path: '',  component: WelcomeComponent},
+  {path: 'wall',  component: WallComponent},
+  {path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
