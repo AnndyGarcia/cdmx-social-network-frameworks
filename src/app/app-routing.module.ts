f8c8f2402c30bcadaa0a1aff7f7e7b4b9ed81611
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-10-05 13:17:17 -0500

    Se agrega redireccion de seguridad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, Router } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+
 import { WallComponent } from './components/wall/wall.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
-  {path: '',  component: WelcomeComponent},
-  {path: 'wall',  component: WallComponent},
-  {path: 'profile', component: ProfileComponent}
+  { path: '', component: WelcomeComponent },
+  { path: 'wall', component: WallComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
