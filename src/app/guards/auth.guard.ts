168d9c795f81110e263cfce70a7d0ded2f8cb795
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-10-05 13:14:16 -0500

    Se agrega seguridad a la pagina

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { take, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.afAuth.authState
+      .pipe(take(1))
+      .pipe(map(authState => !!authState))
+      .pipe(tap(authenticated => {
+        if (!authenticated) {
+          this.router.navigate(['']);
+        }
+      }))
+  }
+}
